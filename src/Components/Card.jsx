7b59a4276316f777c985cb0dbbee349562bb6646
merouanezouaid <marwan.zouaid@gmail.com>
2022-06-19 18:36:54 +0100

    fix things

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -12,16 +12,6 @@ function Card() {
     },{
         name: "Sidi Ali",
         quote: "Hello world"
-    },{
-        name: "Sidi Ali",
-        quote: "Hello world"
-    },{
-        name: "Sidi Ali",
-        quote: "Hello world"
-    },
-    {
-        name: "Sidi Ali",
-        quote: "Hello world"
     }]
 
 
@@ -29,7 +19,7 @@ function Card() {
 <Grid className="px-5 md:px-20" container spacing={1} justify="center" columns={{ xs: 4, sm: 8, md: 12 }}>
   {people.map((item, i) => (
     <Grid item xs={4} sm={4} md={4} key={i}>
-        <div className="bg-white rounded-lg border h-full border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="rounded-lg border h-full border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                       <div class="flex flex-col items-center pb-10 text-center">
                     <img class="mt-6 w-24 h-24 rounded-full shadow-lg" src="https://pbs.twimg.com/profile_images/1440277608466300941/YcDf1maL_400x400.jpg" alt={i}/>
                     <h5 class="mt-3 text-xl font-medium text-gray-900 dark:text-white">{item.name}</h5>
