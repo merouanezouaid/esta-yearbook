f8877ec5e570b5b72f6951d7e68d11e8d2279824
merouanezouaid <marwan.zouaid@gmail.com>
2022-06-21 21:48:04 +0100

    markdown iteration

--- src/Components/Yearbook.jsx
+++ src/Components/Yearbook.jsx
@@ -3,22 +3,9 @@ import { ParallaxLayer } from '@react-spring/parallax';
 import Card from './Card'
 import Github from './Github'
 
-import Markdown from 'markdown-to-jsx';
 
 
 function Yearbook() {
-  const [post, setPost] = useState('');
-  useEffect(() => {
-    import("../data/merouanezouaid.md")
-        .then(res => {
-            fetch(res.default)
-                .then(res => res.text())
-                .then(res => setPost(res))
-                .catch(err => console.log(err));
-        })
-        .catch(err => console.log(err));
-});
-
     return (
       <ParallaxLayer offset={1.70} speed={1}>
       <div className="object-cover">
@@ -30,9 +17,6 @@ function Yearbook() {
           EST GI Promo 2022 student?<br/>
           <Github />
           </p>
-          <Markdown>
-                {post}
-            </Markdown>
           
       </div>
 
