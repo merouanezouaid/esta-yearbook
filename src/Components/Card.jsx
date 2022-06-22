5a579fdb333f4b11cf4c9df0557b1ed288a4af2a
merouanezouaid <marwan.zouaid@gmail.com>
2022-06-22 14:53:15 +0100

    fixed cards contrast

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -19,11 +19,11 @@ function Card() {
 <Grid className="px-5 md:px-20" container spacing={1} justify="center" columns={{ xs: 4, sm: 8, md: 12 }}>
   {people.map((item, i) => (
     <Grid item xs={4} sm={4} md={4} key={i}>
-        <div className="rounded-lg border h-full border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="rounded-lg border h-full border-gray-200 shadow-md bg-gray-800">
                       <div class="flex flex-col items-center pb-10 text-center">
                     <img class="mt-6 w-24 h-24 rounded-full shadow-lg" src="https://pbs.twimg.com/profile_images/1440277608466300941/YcDf1maL_400x400.jpg" alt={i}/>
-                    <h5 class="mt-3 text-xl font-medium text-gray-900 dark:text-white">{item.name}</h5>
-                    <p class="text-sm p-3  text-gray-500 dark:text-gray-400">{item.quote}</p>
+                    <h5 class="mt-3 text-xl font-medium text-white">{item.name}</h5>
+                    <p class="text-sm p-3 text-gray-400">{item.quote}</p>
                 </div>
         </div>
     </Grid>
