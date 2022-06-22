5bb2e9eac969458f19266d711106819f28ab49f7
merouanezouaid <marwan.zouaid@gmail.com>
2022-06-22 16:00:58 +0100

    test

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -10,7 +10,7 @@ function Slider() {
 <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <p className="mb-8 text-4xl leading-relaxed">
-          This class was a very amazing place to meet, learn, work, suffer, <s>die</s>, and enjoy together!
+          This class was a very amazing place to meet, learn, work, suffer, and enjoy together!
           </p>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
