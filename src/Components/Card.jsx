cce7568e34f5f65f6c82c54acd492f23060f1503
Merouane Zouaid <74367730+merouanezouaid@users.noreply.github.com>
2022-06-24 14:57:37 +0100

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,12 +5,15 @@ import Grid from '@mui/material/Grid'
 function Card() {
     let people = [{
         name: "Kaito Koruba",
+        picture: "https://pbs.twimg.com/profile_images/1440277608466300941/YcDf1maL_400x400.jpg",
         quote: "Coding is my favorite videogame."
-    }, {
-        name: "Sidi Ali",
-        quote: "Hello world"
+    },{
+        name: "Brahim Elhoube",
+        picture: "https://i.imgur.com/tN9X2gb.jpeg",
+        quote: "PHP is the best language in the world :)." 
     },{
         name: "Sidi Ali",
+        picture: "https://cdn3d.iconscout.com/3d/premium/thumb/graduation-hat-with-certificate-4034005-3337493.png",
         quote: "Hello world"
     }]
 
@@ -21,7 +24,7 @@ function Card() {
     <Grid item xs={4} sm={4} md={4} key={i}>
         <div className="rounded-lg border h-full border-gray-200 shadow-md bg-gray-800">
                       <div class="flex flex-col items-center pb-10 text-center">
-                    <img class="mt-6 w-24 h-24 rounded-full shadow-lg" src="https://pbs.twimg.com/profile_images/1440277608466300941/YcDf1maL_400x400.jpg" alt={i}/>
+                    <img class="mt-6 w-24 h-24 rounded-full shadow-lg" src={item.picture} alt={i}/>
                     <h5 class="mt-3 text-xl font-medium text-white">{item.name}</h5>
                     <p class="text-sm p-3 text-gray-400">{item.quote}</p>
                 </div>
